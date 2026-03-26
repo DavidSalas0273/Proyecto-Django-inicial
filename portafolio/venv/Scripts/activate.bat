b24a527955c37ae93c29262430c66fc53daec571
Alejandro Molina <jhon.jojoa@campusucc.edu.co>
2026-03-26 15:08:15 -0500

    Hoja de vida dev3 terminada

--- portafolio/venv/Scripts/activate.bat
+++ portafolio/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\alejo\Documents\DJANGO EXAMEN\portafolio\venv"
+set "VIRTUAL_ENV=C:\Users\am531\OneDrive\Desktop\Django\Proyecto-Django-inicial\portafolio\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
