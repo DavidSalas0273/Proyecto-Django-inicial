24359ad3f24e9c9bd96c4cdaf99272626e82cb2b
Sebastian David <sebastiandavid98@example.com>
2026-03-26 16:37:39 -0500

    subo portafolio dev 2

--- portafolio/portafolio/urls.py
+++ portafolio/portafolio/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dev2 import views   # 👈 IMPORTANTE
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.hoja_vida, name='home'),
+    ]
